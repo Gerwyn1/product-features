d0e79479de31646408c903304bd2bf2094f67ac0
Gerwyn Sim <gerwynsim@hotmail.com>
2023-12-21 02:50:18 +0800

    change origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import GalleryRoutes from './routes/gallery.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 const app = express();
-app.use(cors({credentials: true, origin: 'http://localhost:5174'}));
+app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
 app.use(cookieParser());
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb'}));
